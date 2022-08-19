12c55dd94888739418e500538adcbbe4f7cbd2c1
Sebouh Aguehian <sebouh.aguehian@sentiance.com>
2022-08-19 17:55:08 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SENTSDK",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "SENTSDK",
+            targets: ["SENTSDK", "SENTSDKExt", "mpde"]),
+        .library(
+            name: "UnzipKit",
+            targets: ["UnzipKit"]),
+        .library(
+            name: "TensorFlowLiteC",
+            targets: ["SENTTensorFlowLiteC"]),
+        .library(
+            name: "ProtocolBuffersObjC",
+            targets: ["ProtocolBuffers"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SENTSDKExt",
+            dependencies: [],
+            resources: [.copy("Resources/SENTSDK.bundle")],
+            linkerSettings: [.unsafeFlags(["-lz", "-all_load", "-lc++"])]
+        ),
+        .binaryTarget(
+            name: "SENTSDK",
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/SENTSDK/???/SENTSDK-spm-???.xcframework.zip",
+            checksum: "???"
+        ),
+        .binaryTarget(
+            name: "SENTTensorFlowLiteC",
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/TensorFlowLiteC/2.7.0/SENTTensorFlowLiteC.xcframework.zip",
+            checksum: "d0919bfed1872291087fafd394b7fe994c7b3cc379b108750e3b01200dafe53d"
+        ),
+        .binaryTarget(
+            name: "UnzipKit",
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/UnzipKit/1.9/UnzipKit.xcframework.zip",
+            checksum: "7f25e1c509552611089599bab6d31fd9965ad9cf6054bc57512ea3ac19dd6c50"
+        ),
+        .binaryTarget(
+            name: "ProtocolBuffers",
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/ProtocolBuffers/3.18.2/ProtocolBuffers.xcframework.zip",
+            checksum: "2faf9c5aecc9ae62ec3fc66aef964c30af548828893f0f46e168dc8850676de6"
+        ),
+        .binaryTarget(
+            name: "mpde",
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/mpde/0.3.6-SNAPSHOT/mpde.xcframework.zip",
+            checksum: "d146b127dc217af6e89ed062e72deb90c287cd2aa17d921ec1d31e256c286fae"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# SENTSDK
+
+Sentiance SDK Swift Package.
+
+### Resources
+* [About Sentiance](https://www.sentiance.com/)
+* [Integration guide](https://docs.sentiance.com/sdk/getting-started/ios-sdk)
