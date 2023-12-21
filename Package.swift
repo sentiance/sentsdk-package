52eb0ffd2f63a77b455dc91c4a4d3c06ec336e10
Circle CI <dev@sentiance.com>
2023-12-21 12:27:23 +0000

    Update the package for v6.6.0-rc1

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         ),
         .library(
             name: "ProtocolBuffersObjC",
-            targets: ["ProtocolBuffers"]
+            targets: ["Protobuf"]
         ),
     ],
     dependencies: [],
@@ -33,8 +33,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SENTSDK",
-            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/SENTSDK/6.5.2/SENTSDK-spm-6.5.2.xcframework.zip",
-            checksum: "b85712f82a3fbbff13f927c4f0ec74d1032d5f542a7965b82327b971f4bd73db"
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/SENTSDK/6.6.0-rc1/SENTSDK-spm-6.6.0-rc1.xcframework.zip",
+            checksum: "1248eb039ff4f8efeeb48448cd8fda6acaf05c310d8316d3a7b465c3e5be10e8"
         ),
         .binaryTarget(
             name: "SENTTensorFlowLiteC",
@@ -47,9 +47,9 @@ let package = Package(
             checksum: "7f25e1c509552611089599bab6d31fd9965ad9cf6054bc57512ea3ac19dd6c50"
         ),
         .binaryTarget(
-            name: "ProtocolBuffers",
-            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/ProtocolBuffers/3.18.2/ProtocolBuffers.xcframework.zip",
-            checksum: "2faf9c5aecc9ae62ec3fc66aef964c30af548828893f0f46e168dc8850676de6"
+            name: "Protobuf",
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/Protobuf/3.18.2/Protobuf.xcframework.zip",
+            checksum: "d67fa67529f1c502ff96f15ff22d79ed654c58f090ff1d36b7eaebac385ffeba"
         ),
         .binaryTarget(
             name: "mpde",
