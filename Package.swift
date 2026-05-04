59be741212f781005f26da9682d2761f574c653c
Circle CI <dev@sentiance.com>
2026-05-04 13:32:47 +0000

    Update the package for v6.25.0

--- Package.swift
+++ Package.swift
@@ -33,8 +33,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SENTSDK",
-            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/SENTSDK/6.25.0-rc1/SENTSDK-spm-6.25.0-rc1.xcframework.zip",
-            checksum: "71f004b15014a6ee1a97e2316a261263f2271a953106fd0b23f2e17a41712e4e"
+            url: "https://sentiance-u1-sdk-downloads.s3.eu-west-1.amazonaws.com/ios/frameworks/SENTSDK/6.25.0/SENTSDK-spm-6.25.0.xcframework.zip",
+            checksum: "1a7f1d5e6469291e087bb648b09923628fb246c708d8f2dff1c3cbab37127a0f"
         ),
         .binaryTarget(
             name: "TensorFlowLiteC",
